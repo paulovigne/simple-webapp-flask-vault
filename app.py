d760ddf2e1d6030ea8e879bdcc0a9aa069626198
paulovigne <57726813+paulovigne@users.noreply.github.com>
2023-10-09 20:03:08 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 import os
+import hvac
 from flask import Flask
 app = Flask(__name__)
 
+client = hvac.Client()
+
+client.auth.approle.login(
+    role_id='<some_role_id>',
+    secret_id='<some_secret_id>',
+)
+
+read_response = client.secrets.kv.read_secret_version(path='app-secrets/simple-webapp-flask')
+app_color = read_response['data']['data']['app_color']
+
 @app.route("/")
 def main():
-    return '<h1><p style="color:' + os.environ.get('APP_COLOR', 'black') + '">Hello World</p></h1>'
+    return '<h1><p style="color:' + app_color + '">Hello World</p></h1>'
 
 @app.route('/api')
 def hello():
