655421d6bd13abbe097346ac82f54496e149309f
paulovigne <57726813+paulovigne@users.noreply.github.com>
2023-10-09 20:20:47 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,21 @@ import hvac
 from flask import Flask
 app = Flask(__name__)
 
+roleid_file=open("/authentication/account.txt","r")
+lines=roleid_file.readlines()
+roleid_file_value=lines[0]
+roleid_file.close()
+
+secretid_file=open("/authentication/account.txt","r")
+lines=secretid_file.readlines()
+secretid_file_value=lines[0]
+secretid_file.close()
+
 client = hvac.Client()
 
 client.auth.approle.login(
-    role_id='<some_role_id>',
-    secret_id='<some_secret_id>',
+    role_id=roleid_file_value,
+    secret_id=secretid_file_value,
 )
 
 read_response = client.secrets.kv.read_secret_version(path='app-secrets/simple-webapp-flask')
