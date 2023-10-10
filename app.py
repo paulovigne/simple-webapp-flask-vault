05e9be82bcc05a5061d9b653f9e1f89d20bd72bc
paulovigne <57726813+paulovigne@users.noreply.github.com>
2023-10-10 09:46:52 -0300

    Update app.py

--- app.py
+++ app.py
@@ -21,11 +21,10 @@ client.auth.approle.login(
     secret_id=getValueFromFile(/tmp/secretid)
 )
 
-read_response = client.secrets.kv.read_secret_version(mount_point='app-secrets', path='simple-webapp-flask')
-app_color = read_response['data']['data']['app_color']
-
 @app.route("/")
 def main():
+    read_response = client.secrets.kv.read_secret_version(mount_point='app-secrets', path='simple-webapp-flask')
+    app_color = read_response['data']['data']['app_color']
     return '<h1><p style="color:' + app_color + '">Hello World</p></h1>'
 
 @app.route('/api')
